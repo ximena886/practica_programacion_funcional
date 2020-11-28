6f555d66a68c0e94a630ed773da3a230820bce59
juan pablo botero <ximenasanchez9988@gmail.com>
2020-11-27 19:39:21 -0500

    programacion funcional practica

---
+++ index.js
@@ -0,0 +1,51 @@
+//Funcion pura 
+/*function multiplicar(numero) { 
+    return numero*5;
+    }
+    let resultado = multiplicar(10);
+    console.log(resultado); */
+
+
+     // comportamiento de la función 
+     /*let totalcompra = 0;
+     function sumarPrecios(cantidad, valor) { 
+         return totalcompra = (totalcompra + (cantidad*valor));
+         }
+         console.log(sumarPrecios(1, 100));
+         console.log(sumarPrecios(1, 100));
+         console.log(sumarPrecios(1, 100));
+         console.log(sumarPrecios(1, 100));
+         console.log(sumarPrecios(1, 100)); 
+         */
+         // función normal 
+         /*function multiplicar(numero) {
+              return numero*numero
+              }
+              console.log(multiplicar(5)); */
+
+               // funcion composición funcional 
+
+              /*function multiplicar(numero) {
+                   return numero*numero
+                   }
+                   function suma(num1, num2) { 
+                       return multiplicar(num1) + multiplicar(num2);
+                       }
+                       console.log(suma(4,5)) */
+
+                       // funciones recursivas 
+
+                       /*function saludar() { 
+                           console.log('Hola mundo, hola chicos'); saludar();
+                           }
+                           saludar();*/
+
+                           // arreglo o iniciar elementos de cada arreglo 
+
+                           /*const listaNumeros = [2,4,6,8,10];
+                           const nuevaLista = []; 
+                           for(numero in listaNumeros) { 
+                               console.log(listaNumeros[numero])
+                                nuevaLista.push(listaNumeros[numero]*2)
+                                 }
+                                 console.log(nuevaLista)*/
